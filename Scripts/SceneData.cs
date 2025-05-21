5c05b2bea6a9866cdbc559bf39f365472528f09f
Brhando <brandonsisco21@gmail.com>
2025-05-21 15:55:20 -0700

    Added a player UI, and SceneData singleton to hold designated spawn points (for now)

---
+++ Scripts/SceneData.cs
@@ -0,0 +1,17 @@
+﻿using Godot;
+using System;
+
+public partial class SceneData : Node
+{
+    public string SpawnPointName = "default";
+
+    public string GetSpawnPointName()
+    {
+        return SpawnPointName;
+    }
+
+    public void ChangeSpawnPointName(string newName)
+    {
+        SpawnPointName = newName;
+    }
+}
