7c224da9b49c4c63d9e2104ad7f716d81030635e
Brhando <brandonsisco21@gmail.com>
2025-05-22 19:01:56 -0700

    Added GameManager Singleton to track the inventory--this is persistent throughout scene changes.

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using Godot;
+using System.Linq;
+using System.Collections.Generic;
+
+public partial class GameManager : Node
+{
+    public static GameManager Instance;
+    [Signal] public delegate void InventoryChangedEventHandler();
+
+    // Dictionary to store item names and their quantities
+    public Dictionary<string, int> Inventory = new();
+    
+    //inventory space capacity
+    private int _inventoryCap = 20;
+
+    public override void _Ready()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            QueueFree();
+        }
+    }
+
+    public bool AddItem(string itemName, int amount = 1)
+    {
+        int currentTotal = Inventory.Values.Sum();
+
+        if (currentTotal + amount > _inventoryCap)
+        {
+            GD.Print("Inventory full!");
+            return false;
+        }
+
+        if (Inventory.ContainsKey(itemName))
+            Inventory[itemName] += amount;
+        else
+            Inventory[itemName] = amount;
+
+        GD.Print($"Added {amount} {itemName}. Total: {Inventory[itemName]}");
+        EmitSignal(nameof(InventoryChanged));
+        return true;
+    }
+
+    public int GetItemCount(string itemName)
+    {
+        return Inventory.GetValueOrDefault(itemName, 0);
+    }
+
+    
+}
