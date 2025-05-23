460c0c8ef559bf9729f7a4f775f645450a408db4
Brhando <brandonsisco21@gmail.com>
2025-05-22 19:00:22 -0700

    Added code to toggle the inventory UI.

--- Scripts/ResourceArea0.cs
+++ Scripts/ResourceArea0.cs
@@ -3,5 +3,23 @@ using System;
 
 public partial class ResourceArea0 : Node2D
 {
+    private CanvasLayer _inventoryUi;
+
+    public override void _Ready()
+    {
+        _inventoryUi = GetNode<CanvasLayer>("InventoryUI/CanvasLayer");
+        _inventoryUi.Visible = false;
+    }
+    
+    public override void _Process(double delta)
+    {
+        if (Input.IsActionJustPressed("inventory"))
+        {
+            _inventoryUi.Visible = !_inventoryUi.Visible;
+            GD.Print("Inventory Toggled: " + _inventoryUi.Visible);
+        }
+        
+    }
     
 }
+
