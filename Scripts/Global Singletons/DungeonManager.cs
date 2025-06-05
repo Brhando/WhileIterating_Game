d60287cdd830659e6d907344254a5c2df59971c5
Brhando <brandonsisco21@gmail.com>
2025-06-05 15:51:56 -0700

    DungeonManager now tracks enemies used, and will spawn groups of enemies. Included a function to increase the level of the enemies encountered that will be used upon dungeon completion.

--- Scripts/Global Singletons/DungeonManager.cs
+++ Scripts/Global Singletons/DungeonManager.cs
@@ -6,7 +6,8 @@ public partial class DungeonManager : Node
     public static DungeonManager Instance;
 
     public int CurrentDungeonLevel { get; private set; } = 1;
-    public List<EnemyManager.Enemy> ActiveDungeonEnemies = new();
+    public List<List<EnemyManager.Enemy>> ActiveDungeonEncounters = new();
+    private HashSet<string> _usedEnemyNames = new(); // avoid duplicates, used to track which enemies were included in the dungeon for proper leveling
 
     public override void _Ready()
     {
@@ -18,33 +19,45 @@ public partial class DungeonManager : Node
 
     public void GenerateDungeon(int roomCount)
     {
-        ActiveDungeonEnemies.Clear();
+        ActiveDungeonEncounters.Clear();
+        _usedEnemyNames.Clear();
 
+        // Get eligible base enemies for this dungeon level
         var eligible = EnemyManager.Instance.Enemies.FindAll(e => e.Level <= CurrentDungeonLevel);
         var rng = new RandomNumberGenerator();
         rng.Randomize();
 
-        for (int i = 0; i < roomCount; i++)
+        for (var i = 0; i < roomCount; i++)
         {
-            var randomEnemy = eligible[rng.RandiRange(0, eligible.Count - 1)].Clone();
-            ActiveDungeonEnemies.Add(randomEnemy);
+            var groupSize = rng.RandiRange(1, Mathf.Min(4, CurrentDungeonLevel)); // 1–4 enemies per encounter; based on CurrentDungeonLevel
+            var encounter = new List<EnemyManager.Enemy>();
+
+            for (var j = 0; j < groupSize; j++)
+            {
+                var selectedTemplate = eligible[rng.RandiRange(0, eligible.Count - 1)];
+                var clonedEnemy = selectedTemplate.Clone();
+                encounter.Add(clonedEnemy);
+
+                // Track the name of the original template used
+                _usedEnemyNames.Add(selectedTemplate.Name);
+            }
+
+            ActiveDungeonEncounters.Add(encounter);
         }
 
-        GD.Print($"Dungeon Level {CurrentDungeonLevel} generated with {ActiveDungeonEnemies.Count} enemies.");
-    }
-
-    public void IncreaseDungeonLevel()
-    {
-        CurrentDungeonLevel++;
-        GD.Print($"Dungeon Level is now {CurrentDungeonLevel}");
+        GD.Print($"Dungeon Level {CurrentDungeonLevel} generated with {ActiveDungeonEncounters.Count} encounters.");
     }
 
     public void IncreaseEnemiesLevel()
     {
-        foreach (var enemy in ActiveDungeonEnemies)
+        foreach (var enemy in EnemyManager.Instance.Enemies)
         {
-            enemy.LevelUp();
-            EnemyManager.Instance.RefreshSkills(enemy);
+            if (_usedEnemyNames.Contains(enemy.Name))
+            {
+                enemy.LevelUp();
+                EnemyManager.Instance.RefreshSkills(enemy);
+                GD.Print($"Leveled up {enemy.Name} to level {enemy.Level}");
+            }
         }
     }
 }
