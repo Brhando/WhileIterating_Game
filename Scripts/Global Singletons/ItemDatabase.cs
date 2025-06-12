ef33e7ec80420989d216bf685dcef8421d290395
Brhando <brandonsisco21@gmail.com>
2025-06-12 14:17:06 -0700

    corrected Icon paths.

--- Scripts/Global Singletons/ItemDatabase.cs
+++ Scripts/Global Singletons/ItemDatabase.cs
@@ -19,13 +19,13 @@ public partial class ItemDatabase : Node
         //hardcoded for now; will load from JSON file later
         //-1 implies infinity; 0 implies N/A
         AddItem(new ItemData("wood_log", "Wood", ItemType.Material, "A sturdy log of wood.",
-            "res://Assets/Icons/Wood.PNG", 60, 10, 0));
+            "res://Assets/Icons/Wood.png", 60, 10, 0));
         AddItem(new ItemData("stone_chunk", "Stone", ItemType.Material, "A hefty chunk of stone.",
             "res://Assets/Icons/Stone.png", 60, 10, 0));
         AddItem(new ItemData("stone_plate", "Stone Plate", ItemType.Consumable, "Basic defensive material. Increases shields by 5 when used.",
-            "res://Assets/Icons/Wood.PNG", 5, 8, 1));
+            "res://Assets/Icons/Stone Plate.png", 5, 8, 1));
         AddItem(new ItemData("plank", "Plank", ItemType.Material, "Refined wood. Don't make it your best friend. It's better used in upgrades.",
-            "res://Assets/Icons/Stone Plate.png", 20, 10, 1)); //need to decide what this builds into and how it will help the player
+            "res://Assets/Icons/Plank.png", 20, 10, 1)); //need to decide what this builds into and how it will help the player
         AddItem(new ItemData("bandage", "Bandage", ItemType.Consumable, "Heals a small amount",
             "res://Assets/Icons/Bandage.png", 5, 5, 1));
         AddItem(new ItemData("gold", "Gold", ItemType.Currency, "Almost always present and valued. Makes the world go round.",
