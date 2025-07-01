f15df8483a6a10b2260db8e8c618201514c9e5d9
Brhando <brandonsisco21@gmail.com>
2025-07-01 15:04:17 -0700

    Added damage value to bleed. Added Counter values to all debuffs.

--- Scripts/Global Singletons/DebuffData.cs
+++ Scripts/Global Singletons/DebuffData.cs
@@ -18,10 +18,10 @@ public partial class DebuffData : Node
     public Dictionary<DebuffType, Debuff> DebuffLibrary = new();
     private void InitializeDebuffs()
     {
-        DebuffLibrary[DebuffType.Bleed] = new Debuff { Type = DebuffType.Bleed };
-        DebuffLibrary[DebuffType.Deflect] = new Debuff { Type = DebuffType.Deflect };
-        DebuffLibrary[DebuffType.Poison] = new Debuff { Type = DebuffType.Poison };
-        DebuffLibrary[DebuffType.Weak] = new Debuff { Type = DebuffType.Weak };
-        DebuffLibrary[DebuffType.BloodFatigue] = new Debuff { Type = DebuffType.BloodFatigue };
+        DebuffLibrary[DebuffType.Bleed] = new Debuff { Type = DebuffType.Bleed, CountAmt = 2, Damage = 5};
+        DebuffLibrary[DebuffType.Deflect] = new Debuff { Type = DebuffType.Deflect, CountAmt = 1 };
+        DebuffLibrary[DebuffType.Poison] = new Debuff { Type = DebuffType.Poison, CountAmt = 1 };
+        DebuffLibrary[DebuffType.Weak] = new Debuff { Type = DebuffType.Weak, CountAmt = 1 };
+        DebuffLibrary[DebuffType.BloodFatigue] = new Debuff { Type = DebuffType.BloodFatigue, CountAmt = 1 };
     }
 }
